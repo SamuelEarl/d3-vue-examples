b7e4af6abd1df99d0a67c7ec063689c10485b53d
Sam Earl <samueltremain@gmail.com>
2019-10-29 11:53:14 -0500

    Created line chart

--- src/views/LineChart.vue
+++ src/views/LineChart.vue
@@ -20,7 +20,11 @@ export default {
       dataset: [],
       xScale: null,
       yScale: null,
+      xAxis: null,
+      yAxis: null,
       line: null,
+      dangerLine: null,
+      formatTime: null,
     };
   },
   watch: {
@@ -34,17 +38,20 @@ export default {
     async renderCharts() {
       const vm = this;
 
+      this.formatTime = d3.timeFormat("%Y");
+
       // If you try to import a CSV file like you would another module, then you will get errors.
       // See "Problem loading data with D3.js in Vue.js":
       // https://forum.vuejs.org/t/problem-loading-data-with-d3-js-in-vue-js/39360
-      const dataFile = "../../data/mauna_loa_co2_monthly_averages.csv";
+      const dataFile = "/data/mauna_loa_co2_monthly_averages.csv";
 
       // Read in data
       // NOTE: The API for d3.csv has changed in D3 v5. It now uses Promises.
       // Read about it here: http://learnjsdata.com/read_data.html.
-      const data = await d3.csv("./mauna_loa_co2_monthly_averages.csv");
 
-      console.log("DATA:", data);
+// I HAD THIS WORKING ON FRIDAY. CAME BACK MONDAY AND IT WOULD NOT WORK. IT LOOKS LIKE THERE IS SOME ERROR IN THE d3.csv and d3.json FUNCTIONS. FOR SOME REASON THEY ARE NOT READING DATA. THEY ONLY RETURN AN ERROR (SPECIFICALLY, THEY RETURN index.html).
+// I AM GOING TO STOP MESSING AROUND WITH THIS AND MOVE ON WITH MY LIFE. THIS IS VERY INFURIATING!!!
+      const data = await d3.csv(dataFile);
 
       data.forEach(function(d) {
         vm.dataset.push(
@@ -68,17 +75,46 @@ export default {
             return d.date;
           })
         ])
-        .range([0, this.w]);
+        .range([this.padding, this.w]);
 
       this.yScale = d3.scaleLinear()
-        .domain([0, d3.max(this.dataset, function(d) {
-          return d.average;
+        .domain([
+          // This scale does NOT use zero as the baseline.
+          // This is how to set the domain's minimum value to be equal to the minimum value in the
+          // dataset (minus 10 to give the graph a little padding at the bottom):
+          d3.min(this.dataset, function(d) {
+            // Include only valid data values in the line graph
+            if (d.average >= 0) {
+              return d.average;
+            }
+          }) - 10,
+          d3.max(this.dataset, function(d) {
+            return d.average;
         })])
-        .range([this.h, 0]);
+        .range([this.h - this.padding, 0]);
+
+
+      // Define axes
+      // ------------
+      // Define x-axis
+      this.xAxis = d3.axisBottom()
+        .scale(this.xScale)
+        .ticks(10)
+        .tickFormat(this.formatTime);
+
+      // Define y-axis
+      this.yAxis = d3.axisLeft()
+        .scale(this.yScale)
+        .ticks(10);
 
 
       // Define line generator
       this.line = d3.line()
+        // If d.average is a valid value (e.g., if d.average is greater than or equal to zero in
+        // this case), then return true so that the value will be included in the graph of the line.
+        .defined(function(d) {
+          return d.average >= 0 && d.average <= 350;
+        })
         .x(function(d) {
           return vm.xScale(d.date);
         })
@@ -86,21 +122,86 @@ export default {
           return vm.yScale(d.average);
         });
 
+      this.dangerLine = d3.line()
+        .defined(function(d) {
+          return d.average > 350;
+        })
+        .x(function(d) {
+          return vm.xScale(d.date);
+        })
+        .y(function(d) {
+          return vm.yScale(d.average);
+        });
+
+
       // Create reference to SVG element
       this.svg = d3.select("#svg");
 
-      // Create line
+
+      // Create lines
       this.svg.append("path")
         .datum(this.dataset)
         .attr("class", "line")
-        .attr("d", this.line)
-        .attr("fill", "none")
-        .attr("stroke", "gray");
+        .attr("d", this.line);
+
+      this.svg.append("path")
+        .datum(this.dataset)
+        .attr("class", "line danger")
+        .attr("d", this.dangerLine);
+
+
+      // Create axes
+      // The axes should be created after the other elements so that, if there is any overlap
+      // between the elements, the axes are drawn on top of the other elements.
+      this.svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(0, ${this.h - this.padding})`)
+        .call(this.xAxis);
+
+      this.svg.append("g")
+        .attr("class", "axis")
+        .attr("transform", `translate(${this.padding}, 0)`)
+        .call(this.yAxis);
+
+
+      // Draw 350 ppm line
+      this.svg.append("line")
+        .attr("class", "line safe-level")
+        .attr("x1", this.padding)
+        .attr("x2", this.w)
+        .attr("y1", this.yScale(350))
+        .attr("y2", this.yScale(350));
+
+      // Label 350 ppm line
+      this.svg.append("text")
+        .attr("class", "danger-label")
+        .attr("x", this.padding + 20)
+        .attr("y", this.yScale(350) - 7)
+        .text('350 ppm "safe" level');
     }
   }
 };
 </script>
 
 <style lang="stylus" scoped>
+  #svg >>> .line {
+    fill: none;
+    stroke: blue;
+    stroke-width: 0.5;
+  }
 
+  #svg >>> .safe-level {
+    stroke: red;
+    stroke-dasharray: 2, 3;
+  }
+
+  #svg >>> .danger {
+    stroke: red;
+  }
+
+  #svg >>> .danger-label {
+    font-family: Helvetica, sans-serif;
+    font-size: 12px;
+    fill: red;
+  }
 </style>
